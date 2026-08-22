998f85cd29e007f22db7ed18b086a6911cc7e869
Deiner Lares <rafael07-dev>
2026-08-22 09:39:13 -0500

    agregar controlador de solicitudes MaintenanceStaff

--- src/main/java/com/imat/equipment_maintenance_api/infrastructure/adapter/in/rest/MaintenanceStaffController.java
+++ src/main/java/com/imat/equipment_maintenance_api/infrastructure/adapter/in/rest/MaintenanceStaffController.java
@@ -1,4 +1,36 @@
 package com.imat.equipment_maintenance_api.infrastructure.adapter.in.rest;
 
+import com.imat.equipment_maintenance_api.domain.port.in.CreateMaintenanceStaffUseCase;
+import com.imat.equipment_maintenance_api.infrastructure.adapter.in.rest.dto.CreateMaintenanceStaffRequest;
+import com.imat.equipment_maintenance_api.infrastructure.adapter.in.rest.dto.MaintenanceStaffResponse;
+import com.imat.equipment_maintenance_api.infrastructure.adapter.in.rest.mapper.MaintenanceStaffMapper;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/staff/")
 public class MaintenanceStaffController {
+
+    private final CreateMaintenanceStaffUseCase maintenanceStaffUseCase;
+    private final MaintenanceStaffMapper maintenanceStaffMapper;
+
+    public MaintenanceStaffController(CreateMaintenanceStaffUseCase maintenanceStaffUseCase, MaintenanceStaffMapper maintenanceStaffMapper) {
+        this.maintenanceStaffUseCase = maintenanceStaffUseCase;
+        this.maintenanceStaffMapper = maintenanceStaffMapper;
+    }
+
+    @PostMapping("create")
+    public ResponseEntity<MaintenanceStaffResponse> create(@RequestBody CreateMaintenanceStaffRequest request){
+
+        var domain = maintenanceStaffMapper.toDomain(request);
+
+        var created = maintenanceStaffUseCase.execute(domain);
+
+        return new ResponseEntity<>(maintenanceStaffMapper.toResponse(created), HttpStatus.CREATED);
+    }
+
 }
