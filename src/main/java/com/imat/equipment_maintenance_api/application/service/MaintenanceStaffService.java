bb74358976e163020a85d520c5a08e33d5812d2c
Deiner Lares <rafael07-dev>
2026-08-22 09:42:37 -0500

    agregar servicio para implementar caso de uso de maintenanceStaff

--- src/main/java/com/imat/equipment_maintenance_api/application/service/MaintenanceStaffService.java
+++ src/main/java/com/imat/equipment_maintenance_api/application/service/MaintenanceStaffService.java
@@ -1,4 +1,19 @@
 package com.imat.equipment_maintenance_api.application.service;
 
-public class MaintenanceStaffService {
+import com.imat.equipment_maintenance_api.domain.model.MaintenanceStaff;
+import com.imat.equipment_maintenance_api.domain.port.in.CreateMaintenanceStaffUseCase;
+import com.imat.equipment_maintenance_api.domain.port.out.MaintenanceStaffRepositoryPort;
+
+public class MaintenanceStaffService implements CreateMaintenanceStaffUseCase {
+
+    private final MaintenanceStaffRepositoryPort maintenanceStaffRepositoryPort;
+
+    public MaintenanceStaffService(MaintenanceStaffRepositoryPort maintenanceStaffRepositoryPort) {
+        this.maintenanceStaffRepositoryPort = maintenanceStaffRepositoryPort;
+    }
+
+    @Override
+    public MaintenanceStaff execute(MaintenanceStaff maintenanceStaff) {
+        return maintenanceStaffRepositoryPort.save(maintenanceStaff);
+    }
 }
