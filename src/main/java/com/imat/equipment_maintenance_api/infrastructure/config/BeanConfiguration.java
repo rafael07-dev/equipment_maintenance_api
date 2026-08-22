d1c88b329da6de6e692c68e33caa5939991f91f3
Deiner Lares <rafael07-dev>
2026-08-22 09:31:59 -0500

    agregar bean de MaintenanceStaffUseCase

--- src/main/java/com/imat/equipment_maintenance_api/infrastructure/config/BeanConfiguration.java
+++ src/main/java/com/imat/equipment_maintenance_api/infrastructure/config/BeanConfiguration.java
@@ -1,8 +1,11 @@
 package com.imat.equipment_maintenance_api.infrastructure.config;
 
 import com.imat.equipment_maintenance_api.application.service.EquipmentService;
+import com.imat.equipment_maintenance_api.application.service.MaintenanceStaffService;
 import com.imat.equipment_maintenance_api.domain.port.in.CreateEquipmentUseCase;
+import com.imat.equipment_maintenance_api.domain.port.in.CreateMaintenanceStaffUseCase;
 import com.imat.equipment_maintenance_api.domain.port.out.EquipmentRepositoryPort;
+import com.imat.equipment_maintenance_api.domain.port.out.MaintenanceStaffRepositoryPort;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,5 +15,10 @@ public class BeanConfiguration {
     @Bean
     public CreateEquipmentUseCase createEquipmentUseCase(EquipmentRepositoryPort equipmentRepositoryPort) {
         return new EquipmentService(equipmentRepositoryPort);
+    }
+
+    @Bean
+    public CreateMaintenanceStaffUseCase createMaintenanceStaffUseCase(MaintenanceStaffRepositoryPort maintenanceStaffRepositoryPort){
+        return new MaintenanceStaffService(maintenanceStaffRepositoryPort);
     }
 }
