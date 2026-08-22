3108f1040a7def6fbf0a302c0d3b331b5b8237cd
Deiner Lares <rafael07-dev>
2026-08-22 09:45:34 -0500

    agregar adapter de persistencia

--- src/main/java/com/imat/equipment_maintenance_api/infrastructure/adapter/out/persistence/MaintenanceStaffPersistenceAdapter.java
+++ src/main/java/com/imat/equipment_maintenance_api/infrastructure/adapter/out/persistence/MaintenanceStaffPersistenceAdapter.java
@@ -1,4 +1,27 @@
 package com.imat.equipment_maintenance_api.infrastructure.adapter.out.persistence;
 
-public class MaintenanceStaffPersistenceAdapter {
+import com.imat.equipment_maintenance_api.domain.model.MaintenanceStaff;
+import com.imat.equipment_maintenance_api.domain.port.out.MaintenanceStaffRepositoryPort;
+import com.imat.equipment_maintenance_api.infrastructure.adapter.out.persistence.mapper.MaintenanceStaffEntityMapper;
+import com.imat.equipment_maintenance_api.infrastructure.adapter.out.persistence.repository.SpringDataMaintenanceStaffRepository;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MaintenanceStaffPersistenceAdapter implements MaintenanceStaffRepositoryPort {
+
+
+    private final SpringDataMaintenanceStaffRepository maintenanceStaffRepository;
+    private final MaintenanceStaffEntityMapper maintenanceStaffEntityMapper;
+
+    public MaintenanceStaffPersistenceAdapter(SpringDataMaintenanceStaffRepository maintenanceStaffRepository, MaintenanceStaffEntityMapper maintenanceStaffEntityMapper) {
+        this.maintenanceStaffRepository = maintenanceStaffRepository;
+        this.maintenanceStaffEntityMapper = maintenanceStaffEntityMapper;
+    }
+
+    @Override
+    public MaintenanceStaff save(MaintenanceStaff maintenanceStaff) {
+        var saved = maintenanceStaffRepository.save(maintenanceStaffEntityMapper.toEntity(maintenanceStaff));
+
+        return maintenanceStaffEntityMapper.toDomain(saved);
+    }
 }
