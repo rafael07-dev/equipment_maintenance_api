00a93966a456dd36627ff7e738c2730936b2832a
Deiner Lares <rafael07-dev>
2026-08-22 09:40:54 -0500

    agregar repositorio JPA para manejo de persistencia

--- src/main/java/com/imat/equipment_maintenance_api/infrastructure/adapter/out/persistence/repository/SpringDataMaintenanceStaffRepository.java
+++ src/main/java/com/imat/equipment_maintenance_api/infrastructure/adapter/out/persistence/repository/SpringDataMaintenanceStaffRepository.java
@@ -1,4 +1,9 @@
 package com.imat.equipment_maintenance_api.infrastructure.adapter.out.persistence.repository;
 
-public interface SpringDataMaintenanceStaffRepository {
+import com.imat.equipment_maintenance_api.infrastructure.adapter.out.persistence.entity.MaintenanceStaffEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SpringDataMaintenanceStaffRepository extends JpaRepository<MaintenanceStaffEntity, Long> {
 }
