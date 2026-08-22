9c2220424f61691aae3b5c6a68b80a474a8a5bd3
Deiner Lares <rafael07-dev>
2026-08-22 09:35:20 -0500

    agregar dtos para manejar solicitudes http

--- src/main/java/com/imat/equipment_maintenance_api/infrastructure/adapter/in/rest/dto/CreateMaintenanceStaffRequest.java
+++ src/main/java/com/imat/equipment_maintenance_api/infrastructure/adapter/in/rest/dto/CreateMaintenanceStaffRequest.java
@@ -1,4 +1,68 @@
 package com.imat.equipment_maintenance_api.infrastructure.adapter.in.rest.dto;
 
 public class CreateMaintenanceStaffRequest {
+
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String occupation;
+    private boolean availability;
+    private Integer maintenanceCompleted;
+    private String signaturePath;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getOccupation() {
+        return occupation;
+    }
+
+    public void setOccupation(String occupation) {
+        this.occupation = occupation;
+    }
+
+    public boolean isAvailability() {
+        return availability;
+    }
+
+    public void setAvailability(boolean availability) {
+        this.availability = availability;
+    }
+
+    public Integer getMaintenanceCompleted() {
+        return maintenanceCompleted;
+    }
+
+    public void setMaintenanceCompleted(Integer maintenanceCompleted) {
+        this.maintenanceCompleted = maintenanceCompleted;
+    }
+
+    public String getSignaturePath() {
+        return signaturePath;
+    }
+
+    public void setSignaturePath(String signaturePath) {
+        this.signaturePath = signaturePath;
+    }
 }
--- src/main/java/com/imat/equipment_maintenance_api/infrastructure/adapter/in/rest/dto/MaintenanceStaffResponse.java
+++ src/main/java/com/imat/equipment_maintenance_api/infrastructure/adapter/in/rest/dto/MaintenanceStaffResponse.java
@@ -1,4 +1,56 @@
 package com.imat.equipment_maintenance_api.infrastructure.adapter.in.rest.dto;
 
 public class MaintenanceStaffResponse {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String occupation;
+    private boolean availability;
+    private Integer maintenanceCompleted;
+    private String signaturePath;
+
+    public MaintenanceStaffResponse(Long id, String firstName, String lastName, String email, String occupation,
+                                    boolean availability, Integer maintenanceCompleted, String signaturePath) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.occupation = occupation;
+        this.availability = availability;
+        this.maintenanceCompleted = maintenanceCompleted;
+        this.signaturePath = signaturePath;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getOccupation() {
+        return occupation;
+    }
+
+    public boolean isAvailability() {
+        return availability;
+    }
+
+    public Integer getMaintenanceCompleted() {
+        return maintenanceCompleted;
+    }
+
+    public String getSignaturePath() {
+        return signaturePath;
+    }
 }
